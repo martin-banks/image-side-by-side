17f281c59130e6f134de9f6e9f918fc7a4de4da7
Martin Banks <anotherbanksy@gmail.com>
2017-07-21 14:05:24 +1000

    seaprate tempalte elemets, add conditionals to skip missing fields

--- src/template.js
+++ src/template.js
@@ -3,34 +3,39 @@ import Styles from './template.sass'
 
 const standardImage = 'Img400'
 
+const captionTemplate = value => `<p class="${Styles.image__caption}">${value}</p>`
+const creditTemplate = value => `<p class="${Styles.image__credit}">${value}</p>`
+
+const imageTemplate = ({ img, caption, credit }) => `<img 
+	class="${Styles.image}" 
+	src="${img[standardImage]}" 
+	srcset="${createSrcSet(img)}"
+	alt="${caption} / ${credit}" 
+/>`
+
+const titleTemplate = value => `<h2 class="${Styles.header__title}">${value}</h2>`
+const introTemplate = value => `<p class="${Styles.header__intro}">${value}</p>`
+
 function template(content) {
 	const { leftimage, leftcaption, leftcredit, rightimage, rightcaption, rightcredit, title, intro } = content
 
 	return `<section class="${Styles.section}">
 		<div class="${Styles.header}">
-			<h2 class="${Styles.header__title}">${title}</h2>
-			<p class="${Styles.header__intro}">${intro}</p>
+			${title ? titleTemplate(title) : ''}
+			${intro ? titleTemplate(intro) : ''}
 		</div>
 
 		<div class="${Styles.wrapper}">
 			<div class="${Styles.left}">
-				<img class="${Styles.image}" 
-					src="${leftimage[standardImage]}" 
-					srcset="${createSrcSet(leftimage)}"
-					alt="${leftcaption} / ${leftcredit}" 
-				/>
-				<p class="${Styles.image__caption}">${leftcaption}</p>
-				<p class="${Styles.image__credit}">${leftcredit}</p>
+				${leftimage ? imageTemplate({ img: leftimage, caption: leftcaption || '', credit: leftcredit || '' }) : ''}
+				${leftcaption ? captionTemplate(leftcaption) : ''}
+				${leftcredit ? creditTemplate(leftcredit) : ''}
 			</div><!--
 
 			--><div class="${Styles.right}">
-				<img class="${Styles.image}" 
-					src="${rightimage[standardImage]}" 
-					srcset="${createSrcSet(rightimage)}"
-					alt="${rightcaption} / ${rightcredit}" 
-				/>
-				<p class="${Styles.image__caption}">${rightcaption}</p>
-				<p class="${Styles.image__credit}">${rightcredit}</p>
+				${rightimage ? imageTemplate({ img: rightimage, caption: rightcaption || '', credit: rightcredit || '' }) : ''}
+				${rightcaption ? captionTemplate(rightcaption) : ''}
+				${rightcredit ? creditTemplate(rightcredit) : ''}
 			</div>
 		</div>
 
