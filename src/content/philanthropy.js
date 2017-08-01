cb2486dc099a22f14d16aa72fd52b3bb321bebd4
Martin Banks <anotherbanksy@gmail.com>
2017-08-01 16:49:25 +1000

    update content and build

--- src/content/philanthropy.js
+++ src/content/philanthropy.js
@@ -11,12 +11,12 @@ import * as kateGirl from './images/philanthropy/kateGirl'
 import * as dianaMines from './images/philanthropy/dianaMines'
 import * as harryMines from './images/philanthropy/harryMines'
 import * as dianaAfrica from './images/philanthropy/dianaAfrica'
-// import * as harryMines from './images/philanthropy/harryMines'
+import * as harryAfrica from './images/philanthropy/harryAfrica'
 
 
 const Content = {
 	title: 'Then and Now',
-	intro: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore odio id voluptatibus',
+	intro: 'William, Harry, and now Catherine, share Diana’s desire to use their privilege to do good in the world',
 
 	// cards: [
 	// 	card1,
@@ -27,39 +27,38 @@ const Content = {
         "leftcaption": "Diana visits the Tushinskaya Children’s Hospital in Moscow in 1995. ",
         "leftcredit": "AFP",
         "rightimage": kateBabies,
-        "rightcaption": "Catherine and William meet a baby being weighed at Sheway, a charity that helps vulnerable mothers, on a trip to Vancouver, Canada in 2016.",
+        "rightcaption": "Catherine and William meet a baby at Sheway, a Canadian charity for vulnerable mothers, in 2016.",
         "rightcredit": "Getty"
     },
     {
         "leftimage": dianaBoysHostel,
-        "leftcaption": "William visited a hostel run by homeless charity The Passage with his mother and brother in 1993. Picture: Instagram; ",
+        "leftcaption": "William visited a hostel run by homeless charity The Passage with his mother and brother in 1993.",
         "leftcredit": "Instagram",
         "rightimage": willHostel,
-        "rightcaption": "Last year he made a solo visit to the charity, saying:  “The visit I made as a child to this place left a deep and lasting impression upon me.” ",
+        "rightcaption": "Visiting again last year, William said the childhood experience \"left a deep and lasting impression upon me”. ",
         "rightcredit": "Getty"
     },
     {
         "leftimage": dianaGirl,
-        "leftcaption": "A smiling Diana shares a tender moment with Yolanda Robertson, eight, on a visit to Ryde Rehabilitation Centre in Sydney in 1983, left. ",
-        "leftcredit": "left credit",
+        "leftcaption": "Diana shares a tender moment with Yolanda Robertson, eight, on a 1983 visit to Ryde Rehabilitation Centre in Sydney. ",
         "rightimage": kateGirl,
         "rightcaption": "Catherine chats to a young girl in the children’s cancer ward at Sainte-Justine University Hospital in Montreal in 2011.",
         "rightcredit": "Getty"
     },
     {
         "leftimage": dianaMines,
-        "leftcaption": "Harry travels to Angola, right, to see the work of HALO Trust, the mine clearance charity championed by his mother. Pictures:  AP",
-        "leftcredit": "left credit",
+        "leftcaption": "Diana makes headlines on a visit to Angola in 1997 as she campaigns for an international ban on landmines.",
+        "leftcredit": "AP",
         "rightimage": harryMines,
-        "rightcaption": "right caption",
-        "rightcredit": "right credit"
+        "rightcaption": "Harry travels to Angola in 2013 to see the work of HALO Trust, the mine clearance charity championed by his mother.",
+        "rightcredit": "AP"
     },
     {
         "leftimage": dianaAfrica,
         "leftcaption": "Diana serves food at a Red Cross centre in Zimbabwe in 1983.",
         "leftcredit": "Getty",
-        "rightimage": harryMines,
-        "rightcaption": "Harry holds the hand of Mutsu Potsane, four, as they go to plant a fruit tree in the grounds of the Mants’Ase children’s home near Mahles Hoek in Lesotho in 2004.",
+        "rightimage": harryAfrica,
+        "rightcaption": "Harry holds Mutsu Potsane's hand as they go to plant a fruit tree at a children’s home in Lesotho in 2004.",
         "rightcredit": " AP"
     }
 ]
